1f1fefdd190703ac4823cfd5c4c18277cc611fbf
Elio Grieco <grieco@egx.com>
2015-11-21 20:21:29 -0700

    Modify schema for Tag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122011402) do
+ActiveRecord::Schema.define(version: 20151122032022) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -43,6 +43,16 @@ ActiveRecord::Schema.define(version: 20151122011402) do
     t.datetime "updated_at",  null: false
     t.string   "type"
   end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "tagable_id"
+    t.string   "tagable_type"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "tags", ["tagable_type", "tagable_id"], name: "index_tags_on_tagable_type_and_tagable_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
