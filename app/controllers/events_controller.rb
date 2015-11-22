8b16f09e069f86e4b878eadf6e3652389735cc57
Elio Grieco <grieco@egx.com>
2015-11-22 01:58:14 -0700

    Add location field to Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -71,6 +71,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :description, :start_time, :duration, :location_id)
+      params.require(:event).permit(:name, :description, :start_time, :duration, :location)
     end
 end
