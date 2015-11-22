9ac260413387c910159f2e05d9303536a097f57e
JennaConnolly <jjsurfgirl@rocketmail.com>
2015-11-21 18:10:25 -0700

    Added Event Model

---
+++ app/views/events/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @event, :id, :name, :description, :start_time, :duration, :location_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   resources :locations
   resources :organizations
   # The priority is based upon order of creation: first created -> highest priority.
