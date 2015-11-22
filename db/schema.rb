c58a511b2696622ea8f8229909db1d73dfbe976d
Elio Grieco <grieco@egx.com>
2015-11-22 02:38:25 -0700

    Modify schema for Organizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151122084956) do
+ActiveRecord::Schema.define(version: 20151122093741) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20151122084956) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "type"
+    t.string   "location"
   end
 
   create_table "tags", force: :cascade do |t|
